d60111282e67cda0d150eef28767fa176e3d4eb8
KairoWageh <kairo.wageh@gmail.com>
2021-08-28 10:57:32 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL', 'mysql://b5ba699eb37ea9:fe42d973@us-cdbr-east-04.cleardb.com/heroku_f6bf92f03a3a3a7?reconnect=true'),
+            'host' => env('DB_HOST', 'us-cdbr-east-04.cleardb.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_f6bf92f03a3a3a7'),
+            'username' => env('DB_USERNAME', 'b5ba699eb37ea9'),
+            'password' => env('DB_PASSWORD', 'fe42d973'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
